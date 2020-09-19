fe2f9b481c91df4589ff83c5c1c31d37ce9f32e0
Polina Veksler <P.Veksler.FIT.122.20@knute.edu.uagit config --global user.name>
2020-09-19 09:50:20 +0300

    Lab2

---
+++ Lab2/Perimetr.py
@@ -0,0 +1,9 @@
+print("Введіть сторону a:")
+a=float(input())
+print("Введіть сторону b:")
+b=float(input())
+print("Введіть сторону c:")
+c=float(input())
+print("Периметр трикутника дорівнює:")
+p=a+b+c
+print(p)
