6d65fbf5abce51024c543adacd7fa3a83b633d34
Polina Veksler <71499777+Polina515@users.noreply.github.com>
2020-09-30 23:37:14 +0300

    Delete Perimetr.py

--- Lab2/Perimetr.py
+++
@@ -1,9 +0,0 @@
-print("Введіть сторону a:")
-a=float(input())
-print("Введіть сторону b:")
-b=float(input())
-print("Введіть сторону c:")
-c=float(input())
-print("Периметр трикутника дорівнює:")
-p=a+b+c
-print(p)
